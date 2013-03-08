5c5599835e9ef9bdac464893f2ffb6d60f4a283c
Dillon Lara <dillonlara115@fullsail.edu>
2013-03-08 11:45:05 -0600

    initial upload of project to github.  Rails MVC structure is ready to go.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyList::Application.config.secret_token = '4d8321b7812a3a47e382d550a32116586a0a006c2e39584f161953b933c73ec36dff55b76122c5fcc6ff605dee27786f8de37bbe61dbe2954dcacd78e19c29bc'
