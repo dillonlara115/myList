5c312edb4601af173a1e1bcb278b51890f33fbd5
Dillon Lara <dillonlara115@fullsail.edu>
2013-03-20 19:39:26 -0500

    3.1 generated misuse of app link on homepage

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,12 @@
 class ProfilesController < ApplicationController
+	#before_filter :authenticate_user!
   def show
+  	@user = User.find_by_profile_name(params[:id])
+  	if @user
+  		@bands = @user.bands.all
+  		render actions: :show
+  	else
+	  	render file: 'public/404', status: 404, formats: [:html]
+	  end
   end
 end
