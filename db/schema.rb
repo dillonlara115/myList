3a6cd52cc8ee4a1a56ee2a9ca4bb8beb6a9d1715
Dillon Lara <dillonlara115@fullsail.edu>
2013-03-20 21:26:32 -0500

    ran into major issues with my database and authentication while trying to create the user profile page.  Will work on more throughout the next few days but will not be done before peer review.  grade as you must.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130314013259) do
+ActiveRecord::Schema.define(:version => 20130311023516) do
 
   create_table "lists", :force => true do |t|
     t.string   "title"
