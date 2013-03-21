dfc51ce4fe340eae4eb141c25ea94ee1968b85b5
Dillon Lara <dillonlara115@fullsail.edu>
2013-03-20 19:20:56 -0500

    3.1 contact page is up and listed on the home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 MyListDemo::Application.routes.draw do
+  get "contact/new"
+
+  get "contact/contact"
+
+  get "home/index"
+
   devise_for :users
 
   resources :lists
@@ -53,7 +59,7 @@ MyListDemo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'lists#index'
+   root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
